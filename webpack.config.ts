bb8e104fb43d8b576e0764a2e8d69e73b5a8b7d1
Fazendaaa <lucas.carotta@outlook.com>
2019-01-01 19:34:37 -0200

    First commit.

---
+++ webpack.config.ts
@@ -0,0 +1,43 @@
+import { join } from 'path';
+
+const nonRelative = (path: string): string => join(__dirname, `./src/lib/${path}`);
+
+module.exports = {
+    target: 'node',
+    mode: 'development',
+    entry: join(__dirname, 'src/main.ts'),
+    node: {
+        __dirname: false
+    },
+    output: {
+        filename: 'main.js',
+        path: join(__dirname, 'dist')
+    },
+    resolve: {
+        alias: {
+            main: join(__dirname, './src/main.ts')
+        },
+        extensions: [
+            '.js',
+            '.ts',
+            '.tsx'
+        ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.mjs$/,
+                include: /node_modules/,
+                type: 'javascript/auto'
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: [
+                    /node_modules/,
+                    /ci/
+                ]
+            }
+        ]
+    }
+};
